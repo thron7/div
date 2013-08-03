61dfd7cc610ce078f429ffb73c474caf0dbd000f
thron7 <thron7@users.sourceforge.net>
2013-08-03 06:10:54 +0200

    more stuff

--- code/python/functional/underscore.py
+++ code/python/functional/underscore.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 
-# Functions inspired by underscore.js
+# Functions inspired by underscore.js/"Functional JavaScript" (Fogus)
 
+# Pick submap
 def pick(map_, *keys):
     return dict((k,v) for k,v in map_.items() if k in keys)
 
-
+# Omit submap
+def omit(map_, *keys):
+    return dict((k,v) for k,v in map_.items() if k not in keys)
