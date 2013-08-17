44ae4a6bc9cab6ca6dcccb230f35998c3e987ec7
thron7 <thron7@users.sourceforge.net>
2013-08-17 22:39:26 +0200

    add curryN

--- code/python/functional/underscore.py
+++ code/python/functional/underscore.py
@@ -9,3 +9,19 @@ def pick(map_, *keys):
 # Omit submap
 def omit(map_, *keys):
     return dict((k,v) for k,v in map_.items() if k not in keys)
+
+# Curry named arguments
+# usage:
+# def foo(a,b,c):
+#     print a,b,c
+# f1 = curryN(foo,'b',2)
+# f1(1,c=3) # args *before* curried can still be positional
+def curryN(fun, arg, val):
+    kwargs = dict([(arg,val)])
+    def f(*args, **akwargs):
+        akwargs.update(kwargs)
+        print(args, akwargs)
+        return fun(*args, **akwargs)
+    return f
+
+
