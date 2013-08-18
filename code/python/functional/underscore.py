7e17842911f246f5b7e2d9b704a054d3e1a23d7a
thron7 <thron7@users.sourceforge.net>
2013-08-18 23:42:17 +0200

    add actions() and lift()

--- code/python/functional/underscore.py
+++ code/python/functional/underscore.py
@@ -23,9 +23,42 @@ def curryN(fun, arg, val):
         return fun(*args, **kwargs)
     return f
 
+# Pipeline - compose functions where one takes the return value of the previous
 import functools
 def pipeline(seed, *args):
     return functools.reduce(lambda accu,elem: elem(accu),
         args, seed)
+
+# Actions - compose functions that take different 'shapes' as inputs
+# @param acts - list of action functions
+# @param done - last function to extract the final result
+# Adapter data:
+# { 'values' : [] - growing list of individual action results
+#   'state'  : <val> - carries the cumulative result
+# }
+def actions(acts, done):
+    # run individual action
+    def g(stateObj, action):
+        result = action(stateObj.state)
+        values = stateObj.values + [result.answer]
+        return { 'values': values, 'state': result.state }
+    # run all actions
+    def f(seed):
+        init = { 'values' : [], 'state': seed}
+        intermediate = functools.reduce(g, acts, init)
+        keep = filter(None, intermediate.values) 
+        return done(keep, intermediate.state)
+    return f
+
+# Lift - helper for action creation
+def lift(answerFun, stateFun=None):
+    def f(*args):
+        def g(state):
+            ans = answerFun(*([state]+args))
+            s = stateFun(state) if stateFun is not None else ans
+            return {'answer': ans, 'state': s}
+        return g
+    return f
+
 
 
