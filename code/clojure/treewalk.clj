7cb48b18ae426b2f4dfc2f9cf0c87f1a88975286
thron7 <thron7@users.sourceforge.net>
2013-09-01 22:44:04 +0200

    add snippet

--- code/clojure/treewalk.clj
+++ code/clojure/treewalk.clj
@@ -1,1 +1,8 @@
-(println "clojure says hello!")
+;;
+;; Walking the file system tree
+;;
+
+; reading directory
+(def directory (clojure.java.io/file "/path/to/directory"))
+(def files (file-seq directory))
+(take 10 files)
