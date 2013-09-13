28153d7ded8a130e90330c17fefc9d0d78836140
thron7 <thron7@users.sourceforge.net>
2013-09-13 22:23:59 +0200

    alternat. version using built-in RegExp
    - about 7x faster than xregexp version
    - about 15% faster than python version

--- code/nodejs/generator/Scanner.js
+++ code/nodejs/generator/Scanner.js
@@ -4,6 +4,18 @@ var fs = require('fs');
 var XRegExp = require('xregexp').XRegExp;
 var _ = require('underscore');
 
+var categories = [
+  '',   // $0 is matched string, $1,... matched groups
+  'float',
+  'hexnum',
+  'number',
+  'ident',
+  'nl',
+  'white',
+  'mulop',
+  'op',
+];
+
 function Scanner() {
 
   this.patt = XRegExp(
@@ -59,6 +71,59 @@ function Scanner() {
         '', 'xsgm'
   );
 
+  this.patt_1 = RegExp(
+
+         '(' +
+                '\\d*\\.\\d+(?:[eE][+-]?\\d+)?' +
+                '|\\d+[eE][+-]?\\d+' +
+                ')' +
+        '|(0x[0-9A-Fa-f]+)' +
+        '|(\\d+)' +
+        '|([$\\w]+)' +
+        '|(\\n' +
+                ')' +
+        /*
+                '\x0D\x0A \n' +
+                '|\x0A \n' +
+                '|\x0D \n' +
+                ') \n' +
+        '|(?<nl>                # unicode line separators \n' +
+                '\x0D\x0A \n' +
+                '#|\x20\x28      # strange: this is " (" !? \n' +
+                '#|\x20\x29      # strange: this is " )" !? \n' +
+                '|\x0A \n' +
+                '|\x0D \n' +
+                ') \n' +
+        */
+        '|((?:(?:\\s|\\ufeff))+)' +
+        /*
+        '|(?<white> (?:(?:\s|\ufeff)(?<!\n))+)     # white ( + BOM - \n) \n' +
+        */
+        '|(' +
+                '<<=?' +
+                '|>=' +
+                '|<=' +
+                '|===?' +
+                '|!==?' +
+                '|[-+*\\/%|^&]=' +
+                '|>>>?=?' +
+                '|&&' +
+                '|[|^]\\|' +
+                '|\\+\\+' +
+                '|--' +
+                '|::' +
+                '|\\.\\.' +
+                '|//' +
+                '|/\\*' +
+                '|\\*\\/' +
+                ')' +
+        '|(\\W)' +
+        /*
+        '#, re.VERBOSE|re.DOTALL|re.MULTILINE|re.UNICODE) # re.LOCALE?!\n' +
+        */
+        '', 'gm'
+  );
+
   this.tokenize = function(stream) {
 
     var tok, toks = [];
@@ -73,6 +138,26 @@ function Scanner() {
     } , this);
 
     return toks;
+  };
+  
+  this.tokenize_1 = function(stream) {
+
+    var tok, toks = [];
+    var match, tok_type, source;
+
+    while (true) {
+        
+      match = this.patt_1.exec(stream);
+      if (match===null)
+        break;
+      else {
+        tok_type = categories[match.indexOf(match[0],1)];
+        source = match[0];
+        tok = [tok_type, source, match.index, source.length];
+        toks.push(tok);
+      }
+    }
+    return toks;
   }
 }
 
@@ -81,7 +166,7 @@ module.exports = Scanner;
 // -- main --
 function main(fcont) {
   var s = new Scanner();
-  var tokens = s.tokenize(fcont);
+  var tokens = s.tokenize_1(fcont);
   tokens.forEach(function (e) {
     console.log(e);
   });
